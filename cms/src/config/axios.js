7f08a4ed0a365c5e13693ef2b6716ae4bd68e0af
pchaubey25 <chaubeyp@gmail.com>
2025-02-11 17:34:41 +0530

    Update axios.js

--- cms/src/config/axios.js
+++ cms/src/config/axios.js
@@ -26,7 +26,7 @@
 import axios from 'axios';
 
 const baseURL =
-  process.env.NODE_ENV === 'production'
+  process.env.NODE_ENV === 'development'
     ? 'http://localhost:3050' // Backend running locally
     : 'https://veeriveoct5.onrender.com'; // Deployed backend
 
