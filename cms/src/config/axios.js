21d66fcfe3d8e73b8e9c8970fa468b3a1dae9ed8
Harsh6530 <sharmaharsh634@kgpian.iitkgp.ac.in>
2025-07-07 03:55:52 +0530

    URL update

--- cms/src/config/axios.js
+++ cms/src/config/axios.js
@@ -27,7 +27,7 @@ import axios from 'axios';
 const baseURL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3050' // Backend running locally
-    : 'backend-iu1ovyi6b-pankajs-projects-186219f8.vercel.app'; // Deployed backend
+    : 'backend-cms-blue.vercel.app'; // Deployed backend
 
 const axiosInstance = axios.create({
   baseURL,
